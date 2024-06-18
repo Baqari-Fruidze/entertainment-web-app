909812097cc3cd9b7f930a2b06270bbfc9c52b0d
Baqari-Fruidze <baqarifruidze@gmail.com>
2024-06-18 23:52:26 +0400

    bookmark movies done

--- src/pages/Bookmarked.tsx
+++ src/pages/Bookmarked.tsx
@@ -1,5 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
+import styled from "styled-components";
+import { Context } from "../App";
+import Input from "../components/Input";
+import BookmarkedMovies from "../components/BookmarkedMovies";
 
 export default function Bookmarked() {
-  return <div></div>;
+  const { data } = useContext(Context);
+  return (
+    <Parent>
+      <Input />
+      <BookmarkedMovies />
+    </Parent>
+  );
 }
+
+const Parent = styled.div`
+  padding: 2.6rem 1.6rem 2rem 1.6rem;
+`;
